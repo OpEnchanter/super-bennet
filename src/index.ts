a9dab57812199953ba5414a320e986698510d13b
Tristan Greenhagen <openchanter@gmail.com>
2026-07-13 23:26:21 -0400

    fix: texture flipping never set texture.needsUpdate

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import * as Phoenix from "phoenix";
 import * as GameScene from "./scenes/game.ts";
 import * as THREE from "three";
+import * as pl from "planck";
 
 const app: Phoenix.App = new Phoenix.App({
-    renderScale: new Phoenix.Vector2(1920, 1080),
+    renderScale: new Phoenix.Vector2(2160, 1440),
     clearColor: 0x4aeddc,
-    zoom: 1/2
+    zoom: 1/4
 })
+
+app.plWorld.setGravity(pl.Vec2(0, -16))
 
 app.addScene("game", new GameScene.Scene())
 app.loadScene("game")
--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -1,19 +1,35 @@
 import * as Phoenix from "phoenix";
-import { Transform } from "planck";
+import { LevelLoader } from "../lib/Loader";
 
 class PlayerController extends Phoenix.Component {
     rigidbody: Phoenix.Rigidbody | undefined = undefined;
+    transform: Phoenix.Transform | undefined = undefined;
+    sprite: Phoenix.AnimatedSprite | undefined = undefined;
+    moveSpeed: number;
+    airMoveSpeed: number;
+    jumpForce: number;
+
+    constructor (moveSpeed: number, airMoveSpeed: number, jumpForce: number) {
+        super()
+        this.moveSpeed = moveSpeed;
+        this.airMoveSpeed = airMoveSpeed;
+        this.jumpForce = jumpForce;
+    }
 
     public override onInitialized(): void {
         this.rigidbody = this.parent?.getComponent(Phoenix.Rigidbody);
+        this.transform = this.parent?.getComponent(Phoenix.Transform);
+        this.sprite = this.parent?.getComponent(Phoenix.AnimatedSprite);
     }
 
     public override onUpdate(): void {
-        if (!this.rigidbody) return;
+        if (!this.rigidbody || !this.transform || !this.sprite) return;
+
+        const movementSpeed = this.parent?.isTriggered ? this.moveSpeed : this.airMoveSpeed;
         
         if (this.parent?.app.getKey("a")) {
             this.rigidbody.body?.applyLinearImpulse(
-                {x: -1, y: 0},
+                {x: -movementSpeed, y: 0},
                 {
                     x: this.rigidbody.body.getPosition().x,
                     y: this.rigidbody.body.getPosition().y
@@ -21,7 +37,7 @@ class PlayerController extends Phoenix.Component {
             )
         } else if (this.parent?.app.getKey("d")) {
             this.rigidbody.body?.applyLinearImpulse(
-                {x: 1, y: 0},
+                {x: movementSpeed, y: 0},
                 {
                     x: this.rigidbody.body.getPosition().x,
                     y: this.rigidbody.body.getPosition().y
@@ -29,15 +45,24 @@ class PlayerController extends Phoenix.Component {
             )
         }
 
-        if (this.parent?.app.getKey("w")) {
+        if (this.parent?.app.getKey("w") && this.parent.isTriggered) {
             this.rigidbody.body?.applyLinearImpulse(
-                {x: 0, y: 3},
+                {x: 0, y: this.jumpForce},
                 {
                     x: this.rigidbody.body.getPosition().x,
                     y: this.rigidbody.body.getPosition().y
                 }
             )
         }
+
+        if (Math.abs(this.rigidbody.body!.getLinearVelocity().x) > 0) {
+            this.sprite.setAnimation("running");
+        } else {
+            this.sprite.setAnimation("standing")
+        }
+
+        this.transform.globalPosition.x = Math.floor(this.transform.globalPosition.x / 4) * 4
+        this.transform.globalPosition.y = Math.floor(this.transform.globalPosition.y / 4) * 4
     }
 }
 
@@ -62,35 +87,48 @@ class CameraController extends Phoenix.Component {
     public override onUpdate(): void {
         if (!this.transform || !this.targetTransform || !this.targetRigidbody) return;
 
-        const targetX = this.targetTransform.globalPosition.x + this.targetRigidbody.body!.getLinearVelocity().x * 3;
-        const targetY = this.targetTransform.globalPosition.y + this.targetRigidbody.body!.getLinearVelocity().y * 3;
+        const targetX = this.targetTransform.globalPosition.x + this.targetRigidbody.body!.getLinearVelocity().x * 6;
+        const targetY = this.targetTransform.globalPosition.y + this.targetRigidbody.body!.getLinearVelocity().y * 6;
         
         this.transform.position.x += 
-            (targetX - this.transform.globalPosition.x) / 4
+            (targetX - this.transform.globalPosition.x) / 8
 
         this.transform.position.y += 
-            (targetY - this.transform.globalPosition.y) / 4
+            (targetY - this.transform.globalPosition.y) / 8
     }
 }
 
 export class Scene extends Phoenix.Scene {
     override onLoad(app: Phoenix.App): void {
+        const animFrames = {
+            "standing": ["assets/bennet/standing.png"],
+            "running": ["assets/bennet/animation/run/1.png", "assets/bennet/animation/run/2.png"]
+        }
+        const anim = new Phoenix.AnimatedSprite(animFrames, 15);
+        console.log(Object.keys(animFrames)[0])
+
         const player = app.createObject(
             new Phoenix.Transform(
                 new Phoenix.Vector2(0, 0),
                 0,
-                new Phoenix.Vector2(32, 32)
+                new Phoenix.Vector2(-24, 32)
             ),
-            new Phoenix.Sprite("assets/bennet/standing.png"),
+
+            anim,
+            
             new Phoenix.Renderer(0),
 
             new Phoenix.BoxCollider(
-                new Phoenix.Vector2(32, 32)
+                new Phoenix.Vector2(24, 32)
             ),
 
-            new Phoenix.Rigidbody(1, 50, false, true),
+            new Phoenix.BoxCollider(
+                new Phoenix.Vector2(24, 4), true, new Phoenix.Vector2(0, -35)
+            ),
 
-            new PlayerController()
+            new Phoenix.Rigidbody(30, 20, false, true),
+
+            new PlayerController(20, 5, 60)
         );
 
         const camera = app.createObject(
@@ -106,20 +144,19 @@ export class Scene extends Phoenix.Scene {
         app.addObject(player);
         app.addObject(camera);
 
-        app.addObject(
-            app.createObject(
-                new Phoenix.Transform(
-                    new Phoenix.Vector2(0, -32),
-                    0,
-                    new Phoenix.Vector2(512, 32)
-                ),
-                new Phoenix.Sprite("assets/tiles/brick/brick.png"),
-                new Phoenix.Renderer(0),
-
-                new Phoenix.BoxCollider(
-                    new Phoenix.Vector2(512, 32)
-                )
-            )
-        );
+        const loader: LevelLoader = new LevelLoader(app);
+        loader.loadFromJson({
+            objects: [
+                {
+                    type: "tile",
+                    data: {
+                        position: {x:0, y:-1},
+                        scale: {x: 1000, y:10},
+                        sprite: "assets/tiles/brick/brick.png",
+                        hasCollision: true
+                    }
+                }
+            ]
+        })
     }
 }
