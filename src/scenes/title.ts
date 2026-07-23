6d7a551567f34830da5eefb7a68ef438ccbf2e38
Tristan Greenhagen <openchanter@gmail.com>
2026-07-22 22:11:03 -0400

    feat: scene on title screen

--- src/scenes/title.ts
+++ src/scenes/title.ts
@@ -1,5 +1,6 @@
 import * as Phoenix from "phoenix";
 import { Switch } from "../lib/ui/Switch";
+import { LevelLoader } from "../lib/scene/Loader";
 
 class ButtonHoverAnimator extends Phoenix.Component {
     transform: Phoenix.Transform | undefined;
@@ -31,7 +32,7 @@ class ButtonHoverAnimator extends Phoenix.Component {
 
 export class Scene extends Phoenix.Scene {
     public override onLoad(app: Phoenix.App): void {
-        app.args.zoom = 1/4;
+        app.args.zoom = 1/2.5;
         app.resize();
 
         const startButtonText = new Phoenix.TextSprite("Play", {
@@ -124,6 +125,24 @@ export class Scene extends Phoenix.Scene {
 
             new Phoenix.CanvasSprite(canvas),
             new Phoenix.UIRenderer(0)
+        ))
+
+        const levelLoader = new LevelLoader(app);
+        levelLoader.loadFromString('{"objects":[{"type":"tileset","data":{"position":{"x":-2,"y":-1},"scale":{"x":5,"y":11},"sprite":"grass_bricks","hasCollision":true}},{"type":"tile","data":{"position":{"x":-1,"y":3},"scale":{"x":2,"y":4},"sprite":"brick_dark","hasCollision":true}},{"type":"tile","data":{"position":{"x":-1,"y":4},"scale":{"x":1,"y":1},"sprite":"brick_grass_top_left","hasCollision":true}},{"type":"tile","data":{"position":{"x":0,"y":4},"scale":{"x":1,"y":1},"sprite":"brick_grass_top_right","hasCollision":true}},{"type":"tile","data":{"position":{"x":3,"y":-2},"scale":{"x":1,"y":1},"sprite":"brick_grass_top_right","hasCollision":true}},{"type":"tile","data":{"position":{"x":4,"y":-7},"scale":{"x":1,"y":1},"sprite":"brick_grass_top_right","hasCollision":true}},{"type":"tile","data":{"position":{"x":-3,"y":-2},"scale":{"x":1,"y":1},"sprite":"brick_grass_top_left","hasCollision":true}},{"type":"tile","data":{"position":{"x":-4,"y":-5},"scale":{"x":1,"y":1},"sprite":"brick_grass_top_left","hasCollision":true}},{"type":"tile","data":{"position":{"x":-3,"y":-3},"scale":{"x":1,"y":9},"sprite":"brick","hasCollision":true}},{"type":"tile","data":{"position":{"x":-4,"y":-6},"scale":{"x":1,"y":6},"sprite":"brick","hasCollision":true}},{"type":"tile","data":{"position":{"x":3,"y":-3},"scale":{"x":1,"y":9},"sprite":"brick","hasCollision":true}},{"type":"tile","data":{"position":{"x":4,"y":-8},"scale":{"x":1,"y":4},"sprite":"brick","hasCollision":true}},{"type":"dynamic","data":{"position":{"x":1,"y":0},"name":"flower_blue","options":{}}},{"type":"dynamic","data":{"position":{"x":3,"y":-1},"name":"sign","options":{}}},{"type":"dynamic","data":{"position":{"x":-1,"y":5},"name":"flower_red","options":{}}}]}');
+
+        app.addObject(app.createObject(
+            new Phoenix.Transform(
+                new Phoenix.Vector2(
+                    0,
+                    64
+                ),
+                0,
+                new Phoenix.Vector2(
+                    0,
+                    0
+                )
+            ),
+            new Phoenix.Camera()
         ))
     }
 }
